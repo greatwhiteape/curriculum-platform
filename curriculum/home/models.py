66ac99124aa4a6572ef83fd0a26d80a90b12ae46
Seth Dresser <sdresser@gmri.org>
2020-04-03 06:09:53 -0400

    Incremental

--- curriculum/home/models.py
+++ curriculum/home/models.py
@@ -1,8 +1,87 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import (FieldPanel, MultiFieldPanel, PageChooserPanel)
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+    StreamFieldPanel,
+)
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Collection, Page
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class People(index.Indexed, ClusterableModel):
+    first_name = models.CharField("First name", max_length=254)
+    last_name = models.CharField("Last name", max_length=254)
+    job_title = models.CharField("Job title", max_length=254)
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    panels = [
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('first_name', classname="col6"),
+                FieldPanel('last_name', classname="col6"),
+            ])
+        ], "Name"),
+        FieldPanel('job_title'),
+        ImageChooserPanel('image')
+    ]
+
+    search_fields = [
+        index.SearchField('first_name'),
+        index.SearchField('last_name'),
+    ]
+
+    @property
+    def thumb_image(self):
+        # Returns an empty string if there is no profile pic or the rendition
+        # file can't be found.
+        try:
+            return self.image.get_rendition('fill-50x50').img_tag()
+        except:
+            return ''
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
+
+
+@register_snippet
+class FooterText(models.Model):
+    body = RichTextField()
+
+    panels = [
+        FieldPanel('body'),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    class Meta:
+        verbose_name_plural = 'Footer Text'
+
 
 
 class HomePage(Page):
