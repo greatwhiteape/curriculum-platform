9666b0996f7188fbb946fd5efedf4410fd1f5a0b
Seth Dresser <sdresser@gmri.org>
2020-04-27 13:36:13 -0400

    Adding necessary fields to Asset models

--- app/assets/endpoints.py
+++ app/assets/endpoints.py
@@ -5,13 +5,35 @@ from .models import Asset
 
 class AssetsAPIEndpoint(BaseAPIEndpoint):
     model = Asset
-    
+
     body_fields = BaseAPIEndpoint.body_fields + [
         'description',
         'internal_link',
         'external_link',
+        'embed_link',
         'program',
         'asset_type',
         'student_asset',
-        'audience',
+        'program',
+        'time_estimate',
+        'audience_relationship',
+        'standards_relationship',
+        'topic_relationship',
+        'tag_relationship',
+    ]
+
+    listing_default_fields = BaseAPIEndpoint.listing_default_fields + [
+        'description',
+        'internal_link',
+        'external_link',
+        'embed_link',
+        'program',
+        'asset_type',
+        'student_asset',
+        'program',
+        'time_estimate',
+        'audience_relationship',
+        'standards_relationship',
+        'topic_relationship',
+        'tag_relationship',
     ]
