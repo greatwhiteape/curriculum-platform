91b8c0cf77fd61a17e9bc12dc7d15c769802e2c1
Seth Dresser <sdresser@gmri.org>
2020-07-09 13:44:45 -0400

    Static File Cleanup

--- app/curriculum_platform/settings/dev.py
+++ app/curriculum_platform/settings/dev.py
@@ -1,10 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 't2e-puz@-va-he4f_d%37osu(03(fk3s6+hr(70p7jz!4$d6v*'
+DEBUG = False
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
--- app/curriculum_platform/settings/production.py
+++ app/curriculum_platform/settings/production.py
@@ -2,6 +2,7 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['teach.gmri.org']
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 try:
